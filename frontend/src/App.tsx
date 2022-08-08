f0ac22a64f40b4d4cf547e8f7830624ebb784e1b
shaaen26 <69615642+shaaen26@users.noreply.github.com>
2022-08-08 18:48:59 +1200

    add style to search component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { TextField, Button } from '@mui/material';
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
+
+  const [ipAddress, setIpAddress] = useState('');
+  const [locationInfomation, setLocationInfomation] = useState<null | object>({});
+
+  
+
+  const search = () => {};
+
   return (
-    <div className="App">
-      <p>hello</p>
+    <div>
+      <div className='searchPart'>
+        <h1>Search Ip Location</h1>
+        <div className="searchInputPart">
+          <TextField 
+          id="searchIp"
+          label="Enter Ip Address..."
+          variant="standard"
+          value={ipAddress}
+          onChange={(e) => setIpAddress(e.target.value)}
+          size="medium"
+        />
+          <Button variant='text'onClick={() => search()}>
+            <SearchIcon className="searchIcon" />
+          </Button>
+        </div>
+      </div>
+
     </div>
   );
 }
