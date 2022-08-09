08d940355fd8d5d5d6be1f6c43df6776daca9f34
shaaen26 <69615642+shaaen26@users.noreply.github.com>
2022-08-09 22:45:08 +1200

    start testing

---
+++ backend/MSA.Phrase2.AmazingAPI/MSA.Phrase2.UnitTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MSA.Phrase2.UnitTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
